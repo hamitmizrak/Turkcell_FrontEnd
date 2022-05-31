2c798ba638f09d067a8616b01106e7a5c04f256b
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-31 12:05:48 +0300

    variable , operator

--- tutorials/js/template.js
+++ tutorials/js/template.js
@@ -7,3 +7,82 @@ multi-line comment
 // alert("Deneme")
 // window.alert("deneme window")
 
+////////////////////////////////////////////////
+// window   >> Hepsi
+// document >> HTML
+
+// alert("pop-up")
+// window.alert("window pop-up")
+// console.log("asd")
+// document.write("Deneme")
+
+////////////////////////////////////////////////
+//Kullanıcıdan veri almak istersek
+// var name=prompt("Adınızı giriniz");
+// console.log(name)
+
+////////////////////////////////////////////////
+//Variable
+// var isim="Hamit";
+// isim="Hamit M"; //string
+// isim=false;  //boolean
+// isim=44; //number
+//console.log(isim);
+
+//var soyisim="Mızrak";
+// soyisim44="Mızrak";
+// $soyisim44="Mızrak";
+// _soyisim44="Mızrak";
+
+//üğşçöÜĞŞÇÖİ değişkenler oluşturmayalım
+//~~soyisim44="Mızrak";//özel ile başlayamayız
+// 44soyisim="Mızrak"; //sayı ile başlayamayız
+
+//camelCase ile değişken adlarını oluşturalım
+//unique 
+//ingilizce kelimeler
+//console.log(_soyisim44)
+
+////////////////////////////////////////////////
+//JS  " " , ''  , ` ` ,  " ' ' "
+
+//variable initialization
+// var deneme;  //undefined
+// console.log(deneme)
+
+////////////////////////////////////////////////
+//Operator
+//logic operators       : &&=VE  ||=VEYA 
+//mathematics operators : + - / * % 
+//comparison operators: > >= < <= ! ==  ===
+//postfix ==>  x++ ,  ++x ,  y-- ,  --y
+
+// 1 byte = 8 bit 
+// bit < byte < GB < TB
+// 1 bit =0
+// Binary codes (2) ==> (0 1)
+// octal (8)        ==> (01234567)
+// decimal (10)     ==> (0123456789)
+// hexadecimal (16) ==> (0123456789abcdef)
+
+////////////////////////////////////////////////
+
+// =   atama 
+// ==  eşit mi (ancak türüne bakma)  "5" 5 TRUE
+// === eşit mi (türüne bak)          "5" 5 FALSE
+
+var result1="Malatya";
+console.log(typeof result1)  //type: türü bulmak
+
+var result2=44.36;
+console.log(typeof result2)  //type: türü bulmak
+
+var result3=true;
+console.log(typeof result3)  //type: türü bulmak
+
+
+
+
+
+
+
