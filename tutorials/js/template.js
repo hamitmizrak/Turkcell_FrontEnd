3b2545240c39e5a849d5c0f58ce1c107da8ba36d
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-01 11:59:20 +0300

    String Ödevi

--- tutorials/js/template.js
+++ tutorials/js/template.js
@@ -265,6 +265,20 @@ document.write("charAt: "+kelime.charAt("0")+"<br>")
 document.write("indexOf: "+kelime.indexOf("css3")+"<br>")
 document.write("lastIndexOf: "+kelime.lastIndexOf("css3")+"<br>")
 
+//CAST
+//String()
+
+//ÖDEV
+// kullanıcı tarafından girilen bir kelimenin
+//S-1) Kaç karakterlidir ?
+//S-2) boşluklar alınarak Kaç karakterlidir ?
+//S-3) bütün kelimeyi küçük harfle göstermek ?
+//S-4) bütün kelimeyi büyük harfle göstermek ?
+//S-5) ilk harf nedir  ?
+//S-6) girdiğiniz kelime java ile başlıyor mu  ?
+//S-7) girdiğiniz kelimenin sonuna "-ben js öğreniyorum"   ?
+//S-8) girdiğiniz 0 ile 4 arasındaki indexi ekranda gösterin   ?
+//S-9) Kullanıcıdan alınan başka bir kelimeyle birinci olan kelimeyle yer değiştirmek ?
 
 
 
