9f4d4e23f771435339c9a5527ddf45f533d4baaa
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-03 10:21:14 +0300

    homework

--- tutorials/js/template.js
+++ tutorials/js/template.js
@@ -527,10 +527,10 @@ multi-line comment
 
 // document.writeln('<br>' + sum3 + '<br>')
 
+//////////////////////////////////////////////////////
 //ÖDEV (if-else for break continue)
-//Fonksiyonlarla
-//Örnek: 1 ile kullanıcının vereceği (prompt) bitiş sayısına göre
-//kullancı: 5 verdi diyelim 1<=X<=5
+//Fonksiyonlarla yapılsın
+//Örnek-1)  kullanıcının vereceği (prompt) bitiş sayısını belirleyelim Exam 1<=X<=5
 //1.adım: kaç tane sayı var ?
 //2.adım: sayı toplamları var ?
 
@@ -547,6 +547,79 @@ multi-line comment
 //Eğer kullanıcı başlangıç sayıdan küçük girerse uyaralım başlangıçtan büyük girmesini isteyelim
 //Eğer kullanıcı secret-key girerse yani 44 sayısını girerse program çalışmayı direk durdursun
 
+//UserData function
+function userData() {
+  var number = Number(prompt('Lütfen bitiş sayısını giriniz'))
+  if (number <= 1) {
+    return "lütfen 1'den büyük sayı giriniz"
+  } else return number
+}
+
+//main function
+function main() {
+  var number = userData();
+  //document.write("<br>"+number);
+  var sum = 0,
+    counter = 0 //toplam ve sayı adedi
+  var even = 0,
+    evenSum = 0,
+    evenNumber = 0 //çift sayılar
+  var odd = 0,
+    oddSum = 0,
+    oddNumber = 0 //Tek sayılar
+
+  for (let i = 1; i <= number; i = i + 1) {
+    //secretKey
+    if (number === 44) {
+      alert(number + ' SecretKey verildi Çıkış yapıldı')
+      break
+    }
+
+    //100 fazla verilemez
+    if (i === 100) {
+      alert('Sayı:' + number + '. 100 fazla hesaplayamam.')
+      break;
+    }
+
+    //special data :7
+    if (i === 7) {
+      alert('Sayı:' + number + ' dahil etmiyorum')
+      continue
+    }
+
+    //toplam
+    sum += i
+    //sum=sum+i;
+    //sum++;
+
+    //çift
+    if (i % 2 == 0) {
+      evenSum += i
+      even++
+      evenNumber = String(evenNumber).concat('-' + i)
+    }
+
+    //çift
+    if (i % 2 == 1) {
+      oddSum += i
+      odd++
+      oddNumber = String(oddNumber).concat('-' + i)
+    }
+  }
+
+  document.write('<br>' + 'verilen Sayı: ' + number)
+  document.write('<br>' + 'verilen Sayı Toplamı: ' + sum + '<br>')
+
+  document.write('<br>' + 'çift Sayı adeti: ' + even)
+  document.write('<br>' + 'çift Sayı toplamı: ' + evenSum)
+  document.write('<br>' + 'çift Sayı nelerdir: ' + evenNumber + '<br>')
+
+  document.write('<br>' + 'tek Sayı adeti: ' + odd)
+  document.write('<br>' + 'tek Sayı toplamı: ' + oddSum)
+  document.write('<br>' + 'tek Sayı nelerdir: ' + oddNumber)
+}
+
+main()
 
 //////////////////////////////////////////////////////
 /// Debug nasıl atılır ?
