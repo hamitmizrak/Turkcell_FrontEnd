439f93df14523dd5711358e54333cfd5cd9ac3f7
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-06 10:32:08 +0300

    dizi

--- tutorials/js/template.js
+++ tutorials/js/template.js
@@ -858,16 +858,19 @@ function arraySplit(){
  }
 
 }
-
 //arraySplit()
 
-// bilgisayar tarafında rastgele oluşturmuş kullanıcı tarafından bitiş sayısına göre sayılar oluşturulsun
+//ÖDEV: Math.round(Math.random()*10+1))
+// bilgisayar tarafında rastgele  sayılar 1-10 arasında rastgele olsun oluştursun ve bitiş değeri 1<=X<=kullanıcı tarafından bitiş sayısına göre sayılar oluşturulsun 
 // bu sayıları bir diziye atama yapalım
-// bu sayılardan ilk eleman
-// bu sayılardan son eleman
-// bu sayıların küçükten büyüğe sıralama
-// bu sayıların büyükten küçüğe sıralama
-// bu sayıların toplamları 
-// bu sayıların çift sayı toplamları 
-// bu sayıların tek sayı toplamları 
-// bu sayıların her birine 1 ekleyerek yeni bir dizi oluşturalım
+// bu sayılardan ilk eleman  ===> dizi[0]
+// bu sayılardan son eleman ===> dizi[dizi.length-1]
+// bu sayıların küçükten büyüğe sıralama ===> dizi.sort()
+// bu sayıların büyükten küçüğe sıralama ===> dizi.sort().reverse()
+// bu sayıların toplamları  ===> sum=0  sum+=i;
+// bu sayıların çift sayı toplamları  ===> if(dizi[i]%2==0)
+// bu sayıların tek sayı toplamları  ===> if(dizi[i]%2==1)
+// bu sayıların her birine 1 ekleyerek yeni bir dizi oluşturalım ==> iterative for ile her bir eleman erişmek ve 1 eklemek bunun dinamik kısmını ES kısmında göreceğiz.
+
+
+//splice()
