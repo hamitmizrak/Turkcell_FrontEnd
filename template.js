74c6ea83d696fc4969c546b3fc679ec9742994a9
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-08 12:02:24 +0300

    Show -Hide date

--- template.js
+++ template.js
@@ -1,33 +1,46 @@
+$(function () {
+  // auto-complete
+  var availableTags = [
+    'ActionScript',
+    'AppleScript',
+    'Asp',
+    'BASIC',
+    'C',
+    'C++',
+    'Clojure',
+    'COBOL',
+    'ColdFusion',
+    'Erlang',
+    'Fortran',
+    'Groovy',
+    'Haskell',
+    'Java',
+    'JavaScript',
+    'Lisp',
+    'Perl',
+    'PHP',
+    'Python',
+    'Ruby',
+    'Scala',
+    'Scheme',
+  ]
+  $('#tags').autocomplete({
+    source: availableTags,
+  })
 
 
-$( function() {
 
-    // auto-complete
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $("#tags").autocomplete({
-      source: availableTags
-    });
-  } );
+
+  //date
+  let showDate=()=>{
+      return new Date();
+  }
+
+//toggle show/hide
+$("#date_hide").html(showDate())
+$("#show_hide_toggler").click(function(){
+    $("#date_hide").toggle(1000)
+})
+
+
+})
